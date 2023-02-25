6f1cbf411b8549cbae3a5cdadab3ae0b898349e8
KARRI KAVYA <113761789+20A31A04P6@users.noreply.github.com>
2023-02-25 19:36:54 +0530

    Add files via upload

---
+++ py22.py
@@ -0,0 +1,10 @@
+'''create a class or variable (name,cost ,model,milege) and print details of the car'''
+class car:
+    name="maruthi"
+    model="shift"
+    cost=1,00,000
+    milege=120
+    def print_details(self):
+        print(f"name:{self.name} and model:{self.model} and cost:{self.cost} and milege:{self.}")
+ob1=car()
+ob1.print_details(self)()       
